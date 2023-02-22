6f231abe9ee4f25cd5db4fc2a701b70e23c01013
SachinShewale <99637286+SachinShewale@users.noreply.github.com>
2023-02-22 14:55:43 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "*****************************my name madu ***********************";
+        return "*****************************my name madutttt ***********************";
     }
 
 }
